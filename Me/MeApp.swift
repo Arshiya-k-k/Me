b5490546a5dc63ddc92c247df0ccc6d46998fb9c
Arshiya <Arshiyakatoch@yahoo.com>
2024-08-02 10:48:19 +0100

    Initial Commit

---
+++ Me/MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeApp.swift
+//  Me
+//
+//  Created by Scholar on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
