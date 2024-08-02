e9db475527c2b82a090115afa7e3595f56a1b4c7
Arshiya <Arshiyakatoch@yahoo.com>
2024-08-02 10:52:44 +0100

    a lot

--- Me/ContentView.swift
+++ Me/ContentView.swift
@@ -1,23 +1,40 @@
 //
 //  ContentView.swift
-//  Me
+//  navigationstacks
 //
-//  Created by Scholar on 02/08/2024.
+//  Created by Scholar on 26/07/2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack{
+                RadialGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/, endRadius: /*@START_MENU_TOKEN@*/500/*@END_MENU_TOKEN@*/)
+                    .ignoresSafeArea()
+                VStack{
+                    
+                    Text("✨All about✨")
+                        .font(.title)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color.black)
+                    
+                       
+                    NavigationLink(destination: profile_page()){
+                        Text("Arshiya")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color(red: 0.316, green: 0.483, blue: 0.907))
+                    }
+                    
+                }
+                
+            }
         }
-        .padding()
-    }
-}
+        }
+        }
+    //closing body//closing struct
 
 #Preview {
     ContentView()
---
+++ Me/ProfilePage.swift
@@ -0,0 +1,83 @@
+//
+//  profile page.swift
+//  navigationstacks
+//
+//  Created by Scholar on 26/07/2024.
+//
+
+import SwiftUI
+
+struct profile_page: View {
+    var body: some View {
+
+     
+        ZStack {
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                Image("me2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(50)
+                    .shadow(radius: 25)
+                
+                
+                HStack(spacing: 80.0) {
+                    Text("Arshiya")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.blue)
+                        .multilineTextAlignment(.center)
+                        .shadow(radius: 2)
+                        .padding()
+                    
+                    VStack{
+                        
+                        Text("17 Years old 🔆")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.blue)
+                            .multilineTextAlignment(.trailing)
+                            .shadow(radius: 3)
+                            
+                        
+                           
+                            
+                        
+                        
+                        Text("soon to be 18!")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.blue)
+                            .multilineTextAlignment(.trailing)
+                            .shadow(radius: 3)
+                        
+                    }
+                    .padding(.trailing, -14.0)
+                }
+                
+                
+                Text("Interests: Travelling, Gel nails, Photography, Badminton,Swimming, Watching netflix and kdramas, hanging out with friends, etc")
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.blue)
+                    .multilineTextAlignment(.center)
+                    .padding([.top, .trailing], -4.0)
+                
+                Text("")
+            }.padding()
+                .background(Rectangle() .foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius: 25)
+                .padding()
+            
+            }
+            
+        }
+        
+    }
+
+#Preview {
+    profile_page()
+}
